cef2f19fc6e153fa509286bb6f870aa328f380af
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2025-01-13 03:06:55 +0100

    initial commit

---
+++ crate2bib/src/lib.rs
@@ -0,0 +1,111 @@
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[derive(Clone, Debug)]
+pub struct BibTex {
+    pub key: String,
+    pub author: String,
+    pub title: String,
+    pub url: Option<String>,
+    pub version: semver::Version,
+    pub date: chrono::DateTime<chrono::Utc>,
+}
+
+impl std::fmt::Display for BibTex {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // Opens the bibtex entry
+        writeln!(f, "@software {{{}", self.key)?;
+        writeln!(f, "    author = {}", self.author)?;
+        writeln!(f, "    title = {}", self.title)?;
+        match &self.url {
+            Some(u) => writeln!(f, "    url = {u}")?,
+            None => (),
+        };
+        writeln!(
+            f,
+            "    date = {}",
+            chrono::DateTime::format(&self.date, "%Y/%m/%d")
+        )?;
+        // Closes the entry
+        writeln!(f, "}}")?;
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug)]
+struct VersionError(String);
+
+impl std::fmt::Display for VersionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)?;
+        Ok(())
+    }
+}
+
+impl std::error::Error for VersionError {
+    fn cause(&self) -> Option<&dyn std::error::Error> {
+        None
+    }
+
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+
+    fn description(&self) -> &str {
+        "Could not find given version number comparator"
+    }
+}
+
+pub fn get_bibtex(crate_name: &str, version: &str) -> Result<BibTex, Box<dyn std::error::Error>> {
+    use crates_io_api::SyncClient;
+    let client = SyncClient::new("my-user-agent", std::time::Duration::from_millis(1000))?;
+    let info = client.get_crate(crate_name)?;
+    let mut obtained_versions = info
+        .versions
+        .iter()
+        .enumerate()
+        .filter_map(|(n, x)| semver::Version::parse(&x.num).ok().map(|y| (n, y)))
+        .collect::<Vec<_>>();
+    obtained_versions.sort_by_key(|x| x.1.clone());
+    obtained_versions.reverse();
+    let version = semver::Comparator::parse(version)?;
+    let (index, found_version_semver) = obtained_versions
+        .into_iter()
+        .find(|x| version.matches(&x.1))
+        .ok_or(VersionError(format!("Could not find {}", version)))?;
+    let found_version = info.versions[index].clone();
+    Ok(BibTex {
+        key: format!("{}{}", crate_name, info.crate_data.updated_at.format("%Y")),
+        author: found_version
+            .published_by
+            .map_or_else(|| format!(""), |x| x.name.unwrap_or(x.login)),
+        title: info
+            .crate_data
+            .description
+            .map_or(crate_name.to_owned(), |x| {
+                format!("{} {}: {}", crate_name, found_version_semver, x)
+            }),
+        url: info.crate_data.repository,
+        version: found_version_semver,
+        date: found_version.updated_at,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn access_crates_io() -> Result<(), Box<dyn std::error::Error>> {
+        let bib_entry = get_bibtex("serde", "1.0.217")?;
+        println!("{}", bib_entry);
+        let expected = "\
+@software {serde2024
+    author = David Tolnay
+    title = serde 1.0.217: A generic serialization/deserialization framework
+    url = https://github.com/serde-rs/serde
+    date = 2024/12/27
+}
+";
+        assert_eq!(format!("{}", bib_entry), expected);
+        Ok(())
+    }
+}
