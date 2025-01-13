d3f260d03b95b3d9d53c4fbee9b1dce5bdc6e62c
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2025-01-13 03:24:50 +0100

    initial commit cli tool

--- crate2bib-cli/src/main.rs
+++ crate2bib-cli/src/main.rs
@@ -1,1 +1,21 @@
-fn main() {}
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(
+    version,
+    about = "Creates a BibTeX entry given a crate name and version number.\
+        Note: This crate respects semver.",
+    long_about = None
+)]
+struct Args {
+    crate_name: String,
+    semver: String,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    let bibtex = crate2bib::get_bibtex(&args.crate_name, &args.semver)?;
+    println!("Generated bibtex:");
+    println!("{bibtex}");
+    Ok(())
+}
