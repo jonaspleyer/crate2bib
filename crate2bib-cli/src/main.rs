94a392aac478bf814522f8bef909db4f27a47561
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2025-01-20 17:30:52 +0100

    add new argumtn for user-agent and adjust output to new get_biblatex function

--- crate2bib-cli/src/main.rs
+++ crate2bib-cli/src/main.rs
@@ -10,12 +10,21 @@ use clap::Parser;
 struct Args {
     crate_name: String,
     semver: String,
+    #[arg(short, long, default_value_t = format!("crate2bib-cli-user-agent"))]
+    user_agent: String,
 }
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    let bibtex = crate2bib::get_bibtex(&args.crate_name, &args.semver).await?;
-    println!("{bibtex}");
+    let (biblatex, origin) =
+        crate2bib::get_biblatex(&args.crate_name, &args.semver, Some(&args.user_agent)).await?;
+    match origin {
+        crate2bib::EntryOrigin::CitationCff => {
+            println!("Obtained from CITATION file in repository")
+        }
+        crate2bib::EntryOrigin::Generated => println!("Obtained from crates.io information"),
+    }
+    println!("{biblatex}");
     Ok(())
 }
