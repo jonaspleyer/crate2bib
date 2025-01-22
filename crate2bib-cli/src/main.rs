a1b03b232212bcb64f079a998cc6e161645fe484
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2025-01-23 00:40:17 +0100

    make version optional; extend description in CLI

--- crate2bib-cli/src/main.rs
+++ crate2bib-cli/src/main.rs
@@ -5,12 +5,20 @@ use clap::Parser;
     version,
     about = "Creates a BibTeX entry given a crate name and version number.\
         Note: This crate respects semver.",
-    long_about = None
+    long_about = None,
+    author,
 )]
 struct Args {
+    /// The exact name of the crate. Note that underscores are synonymous as dashes in the API of
+    /// crates.io.
     crate_name: String,
-    semver: Option<String>,
-    #[arg(short, long, default_value_t = format!("crate2bib-cli-user-agent"))]
+    /// A semver compliant version number (eg. "1", 0.1", "0.3.38").
+    #[arg(short, long, default_value = "")]
+    version: String,
+    /// The name of the user-agent. Automation tools
+    /// should specify this variable to specify which
+    /// user generates the requests.
+    #[arg(short, long, default_value_t = format!("crate2bib-cli-user-agent"), verbatim_doc_comment)]
     user_agent: String,
 }
 
@@ -19,7 +27,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let results = crate2bib::get_biblatex(
         &args.crate_name,
-        args.semver.as_deref(),
+        if args.version.is_empty() {
+            None
+        } else {
+            Some(args.version.as_str())
+        },
         Some(&args.user_agent),
     )
     .await?;
